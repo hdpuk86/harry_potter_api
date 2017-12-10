be53ee02a5b5f28471f0a611ecdbf2f9d72dff54
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-10 14:16:49 +0000

    adds student details

--- public/app.js
+++ public/app.js
@@ -9,7 +9,46 @@ var requestComplete = function(){
   if(this.status !== 200) return;
   var jsonString = this.responseText;
   var students = JSON.parse(jsonString);
-  console.log(students[0]);
+  addStudentDetails(students);
+};
+
+var getStudentFromApi = function(students, studentName){
+  for(student of students){
+    if(student.name === studentName){
+      return student;
+    }
+  }
+};
+
+var addStudentDetails = function(students){
+  var harry = getStudentFromApi(students, "Harry Potter");
+  var ron = getStudentFromApi(students, "Ron Weasley");
+  var hermione = getStudentFromApi(students, "Hermione Granger");
+
+  var harryDiv = document.getElementById('harry');
+  var ronDiv = document.getElementById('ron');
+  var hermioneDiv = document.getElementById('hermione');
+
+  addStudentText(harryDiv, harry);
+  addStudentText(ronDiv, ron);
+  addStudentText(hermioneDiv, hermione);
+};
+
+var addStudentText = function(div, student){
+  var ul = document.createElement('ul');
+  var name = document.createElement('li');
+  name.innerText = student.name;
+  var dob = document.createElement('li');
+  dob.innerText = student.dateOfBirth;
+  var ancestry = document.createElement('li');
+  ancestry.innerText = student.ancestry;
+  var patronus = document.createElement('li');
+  patronus.innerText = student.patronus;
+  ul.appendChild(name);
+  ul.appendChild(dob);
+  ul.appendChild(ancestry);
+  ul.appendChild(patronus);
+  div.appendChild(ul);
 };
 
 var app = function(){
