c7e8b908e43620bb87f6cf72cea27d41c99ca977
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-10 20:12:05 +0000

    adds slytherin

--- public/app.js
+++ public/app.js
@@ -3,25 +3,44 @@ var makeRequest = function(url, callback){
   request.open('GET', url);
   request.addEventListener('load', callback);
   request.send();
-}
+};
 
 var requestComplete = function(){
   if(this.status !== 200) return;
   var jsonString = this.responseText;
   var students = JSON.parse(jsonString);
-  setupGryffindor(students);
+  setupSlytherin(students);
+  setSelectListener(students);
+};
+
+var setSelectListener = function(students){
+  var select = document.getElementById('select-location');
+  select.addEventListener('change', function(event){
+    var index = event.target.selectedIndex;
+    if(index === 1){
+      setupGryffindor(students);
+    } else if (index === 2){
+      setupSlytherin(students);
+    }
+  })
 };
 
 var setupGryffindor = function(students){
   document.getElementById('main').style.backgroundImage = "url('gryffindor_room.jpeg')";
-
-  var portrait = document.getElementById('moving-portrait');
+  var portrait = document.getElementById('moving');
   portrait.src = "dumble.gif";
-
   setupStudent(students, "Harry Potter", "harry.png", "char1");
   setupStudent(students, "Ron Weasley", "ron.png", "char2");
   setupStudent(students, "Hermione Granger", "hermione.png", "char3");
+};
 
+var setupSlytherin = function(students){
+  document.getElementById('main').style.backgroundImage = "url('slytherin.jpeg')";
+  var moving = document.getElementById('moving');
+  moving.src = "";
+  setupStudent(students, "Draco Malfoy", "draco.png", "char1");
+  setupStudent(students, "Lord Voldemort", "voldemort.png", "char2");
+  setupStudent(students, "Severus Snape", "snape.png", "char3");
 };
 
 var setupStudent = function(students, name, imageSrc, studentNumber){
@@ -57,6 +76,7 @@ var removeStudentText = function(div, student){
 var addStudentText = function(div, student){
   var ul = document.createElement('ul');
   ul.id = student.name;
+  ul.className = "student-text";
   var name = document.createElement('li');
   name.innerText = "Name: " + student.name;
   var dob = document.createElement('li');
@@ -73,7 +93,7 @@ var addStudentText = function(div, student){
 };
 
 var app = function(){
-  url = "http://hp-api.herokuapp.com/api/characters/students";
+  url = "http://hp-api.herokuapp.com/api/characters";
   makeRequest(url, requestComplete);
 };
 
